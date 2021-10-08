97d748271392c7ae9aa3b7235712f29ab14a126d
pu0238 <pu0238@protonmail.com>
2021-10-08 09:48:14 +0200

    added mysql connection

--- breakfast/src/app.module.ts
+++ breakfast/src/app.module.ts
@@ -5,9 +5,38 @@ import { AuthModule } from './auth/auth.module';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { User } from './user/user.entity';
+
+dotenv.config();
+const nodeEnvironment = getEnv('NODE_ENV').toUpperCase();
+
+function getEnv(envName: string): string {
+  const value = process.env[envName];
+  if (value) {
+    return value;
+  } else {
+    throw `Could not find environment variable: ${envName}`;
+  }
+}
 
 @Module({
-  imports: [ConfigModule.forRoot({envFilePath: '.env'}), AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    AuthModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: getEnv(`${nodeEnvironment}_DATABASE_HOST`),
+      port: parseInt(getEnv(`${nodeEnvironment}_DATABASE_PORT`)) ?? 3306,
+      username: getEnv(`${nodeEnvironment}_DATABASE_USER`),
+      password: getEnv(`${nodeEnvironment}_DATABASE_PASSWORD`),
+      database: getEnv(`${nodeEnvironment}_DATABASE_NAME`),
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
